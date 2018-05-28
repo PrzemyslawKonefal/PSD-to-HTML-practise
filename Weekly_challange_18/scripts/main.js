326998fbf79a3ff26c72036943ac8bfa4b922c22
Chromor <przemyslawkonefal@gmail.com>
2018-05-29 00:03:07 +0100

    Weekly Challenge 18 website js logic

--- Weekly_challange_18/scripts/main.js
+++ Weekly_challange_18/scripts/main.js
@@ -1,3 +1,46 @@
+$(document).ready(function(){
+
+  function filterProjects(filter){
+      if(filter === "ALL"){
+          $('.portfolio-project').slideDown();
+      }
+      else{
+          $('.portfolio-project').each(function(){
+            if($(this).attr('workType') !== filter){
+                $(this).slideUp();
+            }
+            else{
+                $(this).slideDown();
+            }
+          })
+      }
+  }
+
+  $(".nav-item").click(function(){
+    let destination = 0;
+    if($(this).index() > 0){
+       destination = $("body > section").eq($(this).index()-1).position().top -70;
+      }
+    $('html, body').animate({
+        scrollTop: destination
+    }, 1000);
+  });
+
+  $("#portfolio-panel li").click(function(){
+      $(this).addClass('active-filter');
+      $(this).siblings().removeClass('active-filter');
+      filterProjects($(this).html());
+  });
+
+  $('.portfolio-project').mouseenter(function(){
+    $(this).find('img').css("transform", "scale(1.2,1.2) rotateZ(30deg)");
+  });
+  $('.portfolio-project').mouseleave(function(){
+    $(this).find('img').css("transform", "none");
+  });
+
+});
+
 function myMap() {
   const myCenter = new google.maps.LatLng(44.069447,-69.774520);
     const mapCanvas = document.getElementById("googleMap");
